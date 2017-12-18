118049f12395c78a4332ab0227fcc4a4ede5dc3c
Jeremy Foster <jeremy.foster@live.com>
2017-12-18 13:21:35 -0800

    finish implementing

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import * as moment from 'moment';
 
 let loanStartDate = moment('2011-01-01');
 let months = moment().add(1,'month').diff(loanStartDate,'months');
-console.log(36000 - (months * 300));
+let balance = 36000 - (months * 300);
+let monthsRemaining = balance/300;
+let yearsRemaining = monthsRemaining/12;
+console.log(`Remaining balance: ${balance}`);
+console.log(`Years: ${yearsRemaining}`);
+console.log(`Months: ${monthsRemaining}`);
